edec7c5c4d58ae7199e26551c75bd8c1ad4cdcf9
laura <rosechebet1997@gmail.com>
2022-03-10 13:13:00 -0800

    add bettys

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,13 @@
 /*
  *main - entry point
  *
+ *Description: Prints a list of compilers its compiled on
+ *
  *Return: Always 0 (success)
  */
+
 int main(void)
 {
-
 	char acharacter;
 	int ainteger;
 	long along;
@@ -15,11 +17,9 @@ int main(void)
 	float afloat;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-        printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-
 	return (0);
-
 }
