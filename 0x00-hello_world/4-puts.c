14e018e219b7ab6b7d48b751798776dd7cda5f93
laura <rosechebet1997@gmail.com>
2022-03-10 12:08:36 -0800

    add bettys

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
- 
-/** 
+
+/**
  *main - entry point
  *
  *return 0 :success 
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
