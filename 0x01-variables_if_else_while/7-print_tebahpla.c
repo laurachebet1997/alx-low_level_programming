258e521045f85b9670c8dedcf5199bfc3b7ca3a3
laura <rosechebet1997@gmail.com>
2022-03-11 15:08:10 -0800

    add if while and for

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  *main - Print the alphabet in lowercase letters in reverse
  *
  *
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- *
- *main - Print the numbers of base 16 in lowercase
+ *main - Print the numbers of base in lowercase
  *
  *Return: Always 0 (Success)
  *
@@ -14,17 +13,13 @@ int main(void)
 	char digit;
 
 	for (digit = '0'; digit <= '9'; digit++)
-
 	{
 		putchar(digit);
 	}
-
 	for (digit = 'a'; digit <= 'f'; digit++)
-	
 	{
 		putchar(digit);
 	}
-	
 	putchar('\n');
 	return (0);
 
