49cc7c802db1b4eb41b68969e442d9193e2c7582
laura <rosechebet1997@gmail.com>
2022-03-11 13:04:01 -0800

    add if else while loops

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*printing alphabets*/
+int main(void)
+{
+	char c;
+	for (c='a'; c<='z'; c++)
+		putchar(c);
+	for (c='A'; c<='Z'; c++)
+		putchar(c);
+         	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Prints all the digits using putchar
+ *
+ *Return: Always 0 success
+ *
+ */
+
+int main(void)
+{
+	int digit;
+	for(digit = 0; digit < o; digit++)
+		putchar(digit + '0');
+
+	        putchar('\n');
+	return(o);
+}
