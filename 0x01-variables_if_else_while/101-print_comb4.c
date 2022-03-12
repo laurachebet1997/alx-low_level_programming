2269dc1bd5b8394504f515728bf74cca95e85e06
laura <rosechebet1997@gmail.com>
2022-03-12 01:41:28 -0800

    add if while and for

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int hundreds;
+	int tens;
+	int ones;
+
+	for (hundreds = 0; hundreds <= 0; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= 9; tens++)
+		{
+			for (ones = tens + 1; ones <=9; ones++)
+			{
+				putchar(hundreds + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+
+				if (hundreds < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		putchar('\n');
+		return (0);
