75efb60d2c97b10344645bbc6e23eb94daa09284
laura <rosechebet1997@gmail.com>
2022-03-15 05:00:32 -0700

    use putchar.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 success
+ * *
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = a; i < z; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+	return (0);
+}
