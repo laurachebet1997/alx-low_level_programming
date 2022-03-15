b6a248d81d46e17197a033e93f7c8180f6a4a5af
laura <rosechebet1997@gmail.com>
2022-03-15 11:36:31 -0700

    add timetables

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry points
+ *
+ * @a: Variable a
+ *
+ * @b: Variable b
+ *
+ * Return: Always 0
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * times_table - Entry point
@@ -13,12 +13,14 @@ void times_table(void)
 {
 	int i;
 	int num;
+	int times;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (num = 0; num <= 9; num++)
 		{
-			_putchar(num * i);
+			times = num * i;
+			_putchar(44);
 			_putchar('\n');
 		}
 	}
