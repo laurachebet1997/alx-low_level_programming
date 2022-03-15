b0841e1fc95b02842642b7b7884150f29bbcfaf2
laura <rosechebet1997@gmail.com>
2022-03-15 14:32:28 -0700

    add functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,15 +2,16 @@
 /**
  * print_times_table - Prints the n times table
  *
- * @n: number times table (0 < n <= 15)
+ * @n: number times table
  *
  * Return: no return
  *
- **/
+ */
 
 void print_times_table(int n)
 {
 	int a, b, op;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
