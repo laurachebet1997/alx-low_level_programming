098075fa90aa7d1cf2a95b8a770992b80e936330
laura <rosechebet1997@gmail.com>
2022-03-15 08:28:14 -0700

    add if statements

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: Define a variable
+ *
+ * Return: Always 0
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+}
