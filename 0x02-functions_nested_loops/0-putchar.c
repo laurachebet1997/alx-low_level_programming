66e0bdb6d8d9089912dd6a4d8e94a4eb50eb5488
laura <rosechebet1997@gmail.com>
2022-03-15 03:56:14 -0700

    use putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 success
+ *
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
