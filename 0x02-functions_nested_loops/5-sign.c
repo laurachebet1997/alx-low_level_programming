3992217f79500c6f94ca72a8dc60e22e66ae8ca2
laura <rosechebet1997@gmail.com>
2022-03-15 07:45:59 -0700

    add if statements

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+##include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * @n: Declaring a variable
+ *
+ * Return: Always 0
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(+);
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if(n < 0)
+	{
+		_putchar(-);
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+}
