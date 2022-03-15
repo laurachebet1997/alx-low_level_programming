261ff29f4ad8933026092a046b82e36dbad470a9
laura <rosechebet1997@gmail.com>
2022-03-15 06:12:12 -0700

    find lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 {
 	int i;
 	int times;
-	for (times = 0; times <= 10; times++)
+
+	for (times = 0; times < 10; times++)
 	{
 		for (i = 97; i < 123; i++)
 		{
-			_putchar(i);
+			_putchar(times);
 		}
-		_putchar(times);
 		_putchar('\n');
 	}
 }
