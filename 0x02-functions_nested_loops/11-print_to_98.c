f9f96750b39fe0758ba54d056b7dd04ae9ffc240
laura <rosechebet1997@gmail.com>
2022-03-15 12:15:05 -0700

    add numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: Declaring a variable
+ *
+ * Return: Always 0
+ *
+ */
+
+int print_to_98(int n)
+{
+	for (n = 0 n <= 98 n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+}
