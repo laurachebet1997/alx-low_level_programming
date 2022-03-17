1a224141ca548294a94a87e1af9034ba9d85c1f4
laura <rosechebet1997@gmail.com>
2022-03-17 09:52:20 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,88 +1,20 @@
-#ifndef MAIN_H_
+#ifndef MAIN_H
 
+#define MAIN_H
 
 
-#define MAIN_H_
 
+#include <stdio.h>
 
 
 
+void positive_or_negative(int i);
 
+int largest_number(int a, int b, int c);
 
+int convert_day(int month, int day);
 
-int _putchar(char c);
-
-
-
-void print_alphabet(void);
-
-
-
-void print_alphabet_x10(void);
-
-
-
-int _islower(int c);
-
-
-
-int _isalpha(int c);
-
-
-
-int print_sign(int n);
-
-
-
-int _abs(int);
-
-
-
-int print_last_digit(int);
-
-
-
-void jack_bauer(void);
-
-
-
-void times_table(void);
-
-
-
-void pr_digit(int charcode, int fmt);
-
-
-
-int add(int a, int b);
-
-
-
-void pr_with_fmt(int charcode, int fmt);
-
-
-
-void print_to_98(int n);
-
-
-
-void pr_3_digit(int charcode, int fmt);
-
-
-
-void print_times_table(int n);
-
-
-
-void print_spaces(int n);
-
-
-
-void print_zero(void);
-
-
-
-
+void print_remaining_days(int month, int day, int year);
 
 
 
