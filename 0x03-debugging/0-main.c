9c0100f0988553ada9c4e99da3f2df05c3edc006
laura <rosechebet1997@gmail.com>
2022-03-17 13:45:52 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,13 @@
 
 /**
  * main - tests function prints if integer positive or negative
- * Return: Always 0 
+ * Return: Always 0
  */
 
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
 
