a71713fbcf2598c9d1b00d087a94c22a3684b4c3
laura <rosechebet1997@gmail.com>
2022-03-21 12:41:33 -0700

    add pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_atoi - entry point
+ *
+ *@s:variable
+ *
+ *Return: always 0
+ */
+
+int _atoi(char *s)
+{
+	char s;
+
+	int y = atoi(s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -24,7 +24,7 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
-
+int _atoi(char *s);
 
 
 
