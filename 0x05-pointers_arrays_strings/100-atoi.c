b5cac470fa1ed96386a8772dfe2f8de01055968b
laura <rosechebet1997@gmail.com>
2022-03-21 12:53:16 -0700

    add pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,31 @@
 
 int _atoi(char *s)
 {
-	char s;
 
-	int y = atoi(s);
+	int sign = 1, i = 0;
+
+	unsigned int res = 0;
+
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+
+		if (s[i] == '-')
+
+			sign *= -1;
+
+		i++;
+
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+
+		res = (res * 10) + (s[i] - '0');
+
+		i++;
+
+	}
+	res *= sign;
+	return (res);
+
 }
