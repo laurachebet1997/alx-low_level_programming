dc3be2fe5aaa9838f019b0d5c735678df5ed2fd0
laura <rosechebet1997@gmail.com>
2022-03-23 14:19:52 -0700

    add pointers and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
 	int n;
@@ -9,12 +15,12 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of code here... 
-	 * Remember: 
-	 * - you are not allowed to use a 
-	 * - you are not allowed to modify p 
-	 * - only one statement 
-	 * - you are not allowed to code anything else than this line of code 
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p+5) = 98;
 	/* ...so that this prints 98\n */
