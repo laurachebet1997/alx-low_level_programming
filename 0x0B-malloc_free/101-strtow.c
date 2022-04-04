b389b0cf3d576112896fb053086db3074de83efd
laura <rosechebet1997@gmail.com>
2022-04-04 14:39:36 -0700

    malloc free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
-
 /**
  * strtow - char
  * @str: pointer to string params
  * Return: char
  */
-
 char **strtow(char *str)
 {
 	int i = 0, j = 0, k = 0, len = 0, count = 0;
@@ -20,37 +18,31 @@ char **strtow(char *str)
 				count += 1;
 		i++;
 	}
-	if (count == 0)
+	/*if (count == 0)
 		return (NULL);
-	count += 1;
+	count += 1;*/
 	f = malloc(sizeof(char *) * count);
-	if (!f)
+	if (!f || count == 0)
 		return (NULL);
-	i = 0;
+	/*i = 0;*/
 	while (*str)
 	{
 		while (*str == ' ' && *str)
 			str++;
 		len = 0;
 		while (*(str + len) != ' ' && *(str + len))
-		{
 			len += 1;
-		}
 		len += 1;
 		col = valloc(sizeof(char) * len);
 		if (!col)
 		{
 			for (k = j - 1; k >= 0; k--)
-			{
 				free(f[k]);
-			}
 			free(f);
 			return (NULL);
 		}
 		for (k = 0; k < (len - 1);  k++)
-		{
 			*(col + k) = *(str++);
-		}
 		*(col + k) = '\0';
 		*(f + j) = col;
 		if (j < (count - 1))
