1187f03c868b2c4538587d8c0878aeef422967b6
laura <rosechebet1997@gmail.com>
2022-04-28 13:29:18 -0700

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 		if (index > (8 * sizeof(*n)) - 1)
-					return (-1);
-			*n = *n | (1 << index);
-				return (1);
+		{
+			return (-1);
+		}
+		*n = *n | (1 << index);
+		return (1);
 }
