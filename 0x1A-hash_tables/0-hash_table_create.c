201b2bc80e04e2e3faf6bb06f3e9078266aa3e17
laura <rosechebet1997@gmail.com>
2022-06-30 14:17:59 -0700

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,6 +2,7 @@
 
 /**
  * hash_table_create - create a hash table
+ *
  * @size: size of the table
  * Return: pointer to the table
  */
@@ -10,10 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 
 	if (size == 0)
-	return (NULL);
+		return (NULL);
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
+
 	ht->size = size;
 	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
@@ -21,5 +23,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
+
 	return (ht);
 }
