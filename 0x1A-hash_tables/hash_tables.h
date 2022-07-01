1a21a68c54779bba57ab53faac0ca81a0cf72940
laura <rosechebet1997@gmail.com>
2022-07-01 00:32:45 -0700

    hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,7 +2,7 @@
 #define _HASH_TABLE_H_
 
 #include <stdio.h>
-#include <stdlib.>
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -40,7 +40,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
-void hash_table_print_rev(const hash_table_t *ht)
 void hash_table_delete(hash_table_t *ht);
 
 #endif
